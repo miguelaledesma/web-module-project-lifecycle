140f1ac56f294a65f57300498e41bbaf6ced45da
Miguel A. Ledesma <miguelledesma33@gmail.com>
2022-03-08 12:59:14 -0800

    got test for code grade to pass, going to continue

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,59 @@
 import React from 'react'
+import axios from 'axios'
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+   state = {
+     todos: [], 
+   }
+
+   fetchAllTodos = () => {
+    axios.get(URL)
+    .then(res => {
+      this.setState({
+        ...this.state, 
+        todos: res.data.data
+      })
+    })
+    .catch(err => {
+      debugger
+    })
+
+   }
+
+   componentDidMount() {
+     //fetch all the todos from the server 
+     this.fetchAllTodos()
+   }
+
   render() {
-    return null
+    return (
+      <div>
+      <div id = 'error'> Error: No error here</div>
+      <div id = "todos">
+        <h2>Todos:</h2>
+        {
+          this.state.todos.map(todo => {
+            return <div key ={todo.id} >{todo.name} </div>
+            
+          })
+        }
+      </div>
+      <form id = "todoForm">
+        <input
+         type = 'text'
+         placeholder = 'type todo'
+         /> 
+
+        <input 
+        type = 'submit'
+        />
+
+        <button>Clear Completed</button> 
+      </form>
+
+      </div>
+    )
   }
 }
